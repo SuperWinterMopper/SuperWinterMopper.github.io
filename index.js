8fa909f6586f2fc5c084f2cc641687480d4ecb14
SuperWinterMopper <109165632+SuperWinterMopper@users.noreply.github.com>
2023-08-30 13:02:15 -0500

    First Update
    The site now has the header with buttons for home, SW Songs, Albums. The home page has a large toggle button in the middle for dark mode.

---
+++ index.js
@@ -0,0 +1,35 @@
+let counter = 0;
+
+function slideButton() {
+    counter++;
+    const button = document.querySelector(".sliding-photo-button");
+    const background = document.querySelector(".background");
+    const introduction = document.querySelector(".explanation-text");
+    const darkModeSwitch = document.querySelector(".dark-mode-switch");
+    if(counter % 2 == 1)
+    {
+        button.style.transform = "translateX(300px)";
+        background.style.backgroundColor = "rgb(53, 53, 53)";
+        introduction.style.color = "orange";
+        darkModeSwitch.style.backgroundColor = "rgb(255, 161, 72)";
+    }
+    else 
+    {
+        button.style.transform = "translateX(0px)";
+        background.style.backgroundColor = "aliceblue";
+        introduction.style.color = "rgb(108, 108, 108)";
+        darkModeSwitch.style.backgroundColor = "lightgrey";
+    }
+}  
+
+function linkEssentialAlbums() {
+    window.location.href = "essentialAlbums.html";
+}
+
+function linkStevenWilsonSongs () { 
+    window.location.href = "stevenWilsonSongs.html";
+}
+
+function linkHome() {
+    window.location.href = "index.html";
+}
