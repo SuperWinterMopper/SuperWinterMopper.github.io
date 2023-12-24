1a9e7d08c742ed00b1643b0922f3509d07da472e
SuperWinterMopper <109165632+SuperWinterMopper@users.noreply.github.com>
2023-12-24 17:21:51 -0600

    mobile part.4

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function slideButton() {
     const darkModeSwitch = document.querySelector(".dark-mode-switch");
     if(counter % 2 == 1)
     {
-        console.log("The inner width is " + window.innerWidth);
         if(window.innerWidth <= 600) {
             button.style.transform = "translateX(150px)";
         }
@@ -56,7 +55,6 @@ function moveSliderRight() {
 
 function moveSliderLeft() {
     const sliderContainer = document.querySelector(".slider-container");
-    console.log(currentPosition)
     if(currentPosition < 0)
     {
         currentPosition+=100;
@@ -64,51 +62,59 @@ function moveSliderLeft() {
     }
 }
 
-fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vRUSMvEQ88V3TJ-Czntq-02RwryeStduQjtpJ3lDGOL7sii1G0U8o_JNqYrwlTdWp0ecD6wj0gICFvu/pub?gid=881276312&single=true&output=csv')
-  .then(response => response.text())
-  .then(data => {
-    Papa.parse(data, {
-      header: true, 
-      dynamicTyping: true, 
-      complete: function (results) {
-        results.data.forEach(row => {
-            console.log(row);
-        }
-        )
-
-        results.data.forEach(row => {
-            let counterString = counter2.toString();
-            document.querySelector("#coverArt" + counterString).src = row['Cover'];
-            document.querySelector("#location" + counterString).textContent = row['Artist'];
-            document.querySelector("#date" + counterString).textContent = row['Date'];
-            document.querySelector("#commentary" + counterString).textContent = row['Commentary'];
-            console.log(row);
-
-            let fontSize = 66;
-            //let songTitle = document.querySelector("#song-title" + counterString);
-            if(row['Title'].length > 20)
-            {
-                fontSize = 1168 / row['Title'].replace(/\s/g, '').length;
-                /*while(songTitle.offsetHeight > numberName.offsetHeight || songTitle.offsetWidth > numberName.offsetWidth)
-                {
-                    fontSize--;
-                    songTitle.style.fontSize = fontSize + "px";
-                }*/
-            }
-            document.querySelector("#song-title" + counterString).textContent = row['Title'];
-            document.querySelector("#song-title" + counterString).style.fontSize = fontSize + "px";
-            counter2++;
-        });
-
-        //let specificRow = results.data[1];
-        //document.querySelector("#coverArt" + "2").src = specificRow['Cover'];
-        //console.log(specificRow['Cover']);
-      },
-      error: function (error) {
-        console.error('Error parsing CSV:', error.message);
-      },
-    });
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
+document.addEventListener('DOMContentLoaded', function() {
+    loadSongs();
   });
+
+function loadSongs() {
+    fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vRUSMvEQ88V3TJ-Czntq-02RwryeStduQjtpJ3lDGOL7sii1G0U8o_JNqYrwlTdWp0ecD6wj0gICFvu/pub?gid=881276312&single=true&output=csv')
+    .then(response => response.text())
+    .then(data => {
+        Papa.parse(data, {
+            header: true, 
+            dynamicTyping: true, 
+            complete: function (results) {
+                results.data.forEach(row => {
+                }
+                )
+                
+                results.data.forEach(row => {
+                    let counterString = counter2.toString();
+                    document.querySelector("#coverArt" + counterString).src = row['Cover'];
+                    document.querySelector("#location" + counterString).textContent = row['Artist'];
+                    document.querySelector("#date" + counterString).textContent = row['Date'];
+                    document.querySelector("#commentary" + counterString).textContent = row['Commentary'];
+                    
+                    if(window.innerWidth <= 600) {
+                        let fontSize = 1;
+                        console.log("the window is less than 600 has been called");
+                        if(row['Title'].length > 20)
+                        {
+                            //fontSize = 584 / row['Title'].replace(/\s/g, '').length; this is the right one
+                        }
+                        document.querySelector("#song-title" + counterString).textContent = row['Title'];
+                        document.querySelector("#song-title" + counterString).style.fontSize = fontSize + "px";
+                        counter2++;
+                    }
+                    else {
+                        console.log("Mate the window MORE THAN 600");
+                        let fontSize = 66;
+                        if(row['Title'].length > 20)
+                        {
+                            fontSize = 1168 / row['Title'].replace(/\s/g, '').length;
+                        }
+                        document.querySelector("#song-title" + counterString).textContent = row['Title'];
+                        document.querySelector("#song-title" + counterString).style.fontSize = fontSize + "px";
+                        counter2++;
+                    }
+                });
+            },
+            error: function (error) {
+                console.error('Error parsing CSV:', error.message);
+            },
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+}   
